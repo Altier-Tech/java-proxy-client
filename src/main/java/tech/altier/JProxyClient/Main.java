a9b0cfcc31a533419407d07461fb23530ee667e0
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-01 11:53:43 +0530

    Update Main.java

--- src/main/java/tech/altier/JProxyClient/Main.java
+++ src/main/java/tech/altier/JProxyClient/Main.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tech.altier.Logger.LogManager;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    public static LogManager logger;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("browser-view.fxml"));
@@ -18,6 +21,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        logger = new LogManager();
         launch();
     }
 }

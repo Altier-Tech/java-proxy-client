8e5763aeabd9fc1720bcbfd9b3cafa0514a99689
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-01 11:48:28 +0530

    Create ThreadColor.java

---
+++ src/main/java/tech/altier/Thread/ThreadColor.java
@@ -0,0 +1,26 @@
+package tech.altier.Thread;
+
+/**
+ * This class is used to set the color of the console output.
+ * @author Dilshan Karunarathne
+ * @version 2.0
+ * @since 2020-10-01
+ */
+public class ThreadColor {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_LIGHT_YELLOW = "\u001B[93m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_BOLD = "\u001B[1m";
+    public static final String ANSI_UNBOLD = "\u001B[21m";
+    public static final String ANSI_UNDERLINE = "\u001B[4m";
+    public static final String ANSI_STOP_UNDERLINE = "\u001B[24m";
+    public static final String ANSI_BLINK = "\u001B[5m";
+}

465dad81f785b03f890ed5a5c37a347c3a98a965
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-02 10:45:37 +0530

    Update BuildRequest.java

--- src/main/java/tech/altier/JProxyClient/API/JProxyRequestBuilder/BuildRequest.java
+++ src/main/java/tech/altier/JProxyClient/API/JProxyRequestBuilder/BuildRequest.java
@@ -1,4 +1,23 @@
 package tech.altier.JProxyClient.API.JProxyRequestBuilder;
 
 public class BuildRequest {
+    private final String method;
+    private final String endPoint;
+    private final String version;
+    private final String body;
+
+    public BuildRequest(String method, String endPoint, String version, String body) {
+        this.method = method;
+        this.endPoint = endPoint;
+        this.version = version;
+        this.body = body;
+    }
+
+    public BuildRequest(String endPoint, String body) {
+        this("GET", endPoint, "HTTP/1.1", body);
+    }
+
+    public String build() {
+        return String.format("%s %s %s\r\n\r\n%s", method, endPoint, version, body);
+    }
 }

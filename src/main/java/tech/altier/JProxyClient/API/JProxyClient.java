9e40415d27c8c8c702fac9774fe6dbd0f2387fb0
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-02 11:01:10 +0530

    Update JProxyClient.java

--- src/main/java/tech/altier/JProxyClient/API/JProxyClient.java
+++ src/main/java/tech/altier/JProxyClient/API/JProxyClient.java
@@ -1,7 +1,7 @@
 package tech.altier.JProxyClient.API;
 
 public class JProxyClient {
-    public static void SendRequest(String reqest) {
-        Thread requestThread = new Thread(new JProxyRequest(reqest));
+    public static void SendRequest(String request) {
+        Thread requestThread = new Thread(new JProxyRequest(request));
     }
 }

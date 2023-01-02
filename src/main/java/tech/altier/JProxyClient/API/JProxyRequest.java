f5661498e30257fb974ade36b5a1255f7fb7fed9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-02 11:03:05 +0530

    Update JProxyRequest.java

--- src/main/java/tech/altier/JProxyClient/API/JProxyRequest.java
+++ src/main/java/tech/altier/JProxyClient/API/JProxyRequest.java
@@ -1,17 +1,30 @@
 package tech.altier.JProxyClient.API;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.Socket;
 
 public class JProxyRequest implements Runnable {
     private final String request;
-    HttpURLConnection connection = null;
 
     public JProxyRequest(String request) {
         this.request = request;
+        String host = "www.yourhost.com";
+        Socket socket = new Socket(host, 80);
+
+
+        InputStream is = socket.getInputStream();
+        int ch;
+        while( (ch=is.read())!= -1)
+            System.out.print((char)ch);
+        socket.close();
     }
 
     @Override
     public void run() {
-
+        OutputStream os = socket.getOutputStream();
+        os.write(request.getBytes());
+        os.flush();
     }
 }

63f9dc495a9c10e1e20f17f94fa8ba8a41789377
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-02 10:59:31 +0530

    Update JProxyRequest.java

--- src/main/java/tech/altier/JProxyClient/API/JProxyRequest.java
+++ src/main/java/tech/altier/JProxyClient/API/JProxyRequest.java
@@ -1,4 +1,14 @@
 package tech.altier.JProxyClient.API;
 
 public class JProxyRequest implements Runnable {
+    private final String request;
+
+    public JProxyRequest(String request) {
+        this.request = request;
+    }
+
+    @Override
+    public void run() {
+        
+    }
 }

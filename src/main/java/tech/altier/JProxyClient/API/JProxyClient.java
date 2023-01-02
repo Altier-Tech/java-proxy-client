6672590ac5e57471df0406e8c4a87c17342494d8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-02 11:21:10 +0530

    Update JProxyClient.java

--- src/main/java/tech/altier/JProxyClient/API/JProxyClient.java
+++ src/main/java/tech/altier/JProxyClient/API/JProxyClient.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.*;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
 public class JProxyClient {
